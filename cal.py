fa0b12c062718c956fcde119ba618227e9fc8d9a
Aayushbohora <aayush321a1@gmail.com>
2025-07-07 02:23:14 +0545

    first commit

---
+++ cal.py
@@ -0,0 +1,15 @@
+gh = input("chose any operation : +,-,/,* ")
+num1 = float(input("Enter any number : "))
+num2 = float(input("enter any number : "))
+
+if gh == "+":
+    result = num1+num2
+    print(result)
+elif gh == "-":
+    result = num1-num2
+    print(result)
+elif gh == "*":
+    result = num1*num2
+    print(result)
+
+print("created by Aayush")
